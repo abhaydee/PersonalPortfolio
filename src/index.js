44d86219cd6c07a944504fdbe156ca7d5d69ce9a
abhaydee <abhaygirish1997@gmail.com>
2020-02-05 15:47:13 +0530

    changes for root

--- src/index.js
+++ src/index.js
@@ -11,30 +11,7 @@ import Contact from './Components/Contact';
 const routing=(
     <div>
        <BrowserRouter>
-       <nav className="navbar navbar-expand-lg navbar-dark p-0" style={{'background':'rgb(10, 25, 47)','color':'rgb(100, 255, 218)'}}>
-       <a className="navbar-brand " href="#" routerLink="" style={{'color':'rgb(100, 255, 218)','paddingRight':'1%','borderRight': '1px solid grey','height': '40px'}}>Abhay Deshpande</a>
-       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse navbar navbar-right" id="navbarNav" >
-        <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/" className="nav-link"><img src="https://media-cdn.tripadvisor.com/media/photo-s/13/29/3a/c8/logo.jpg" alt="" width="80"/></Link>
-          
-        </li>
-        <li className="nav-item Resume1" style={{'marginTop': '2%'}}>
-          <Link style={{'color':'rgb(100, 255, 218)'}} to="/Resume" className="nav-link navbar-brand"   id="Resume">Resume</Link>
-        </li>
-        <li className="nav-item Projects1" style={{'marginTop': '2%'}} >
-          <Link style={{'color':'rgb(100, 255, 218)'}} to="/Projects" className="nav-link navbar-brand" id="Projects">Projects</Link>
-        </li>
-        <li className="nav-item contact1" style={{'marginTop': '1%'}} id="contact" >
-          <Link style={{'color':'rgb(100, 255, 218)'}} className="nav-link"  to="/Contact"  >Contact<i className="fa fa-shopping-cart" aria-hidden="true"></i></Link>
 
-        </li>
-        </ul>
-        </div>
-       </nav>
        <Route exact path="/" component={App}></Route>
        <Route exact path="/Resume" component={Resume}></Route>
        <Route exact path="/Projects" component={Projects}></Route>
