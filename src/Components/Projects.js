9240c25db7dfed224723a7be01d948bf12148f1d
abhaydee <abhaygirish1997@gmail.com>
2020-02-06 14:22:12 +0530

    logo's added to Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import html from '../img/html5.png'
-import css from '../img/css.svg'
-import js from '../img/javascript.png'
+import d from '../img/htmllogo.png' 
+import css from '../img/csslogo.jpg'
+import js from '../img/jslogo.png'
 import react from '../img/react.png'
 import reduxsaga from '../img/redux-saga.png'
-import reactnative from '../img/react-native.png'
+import reactnative from '../img/react-native.jpg'
 import angular from '../img/angular.png'
 import node from '../img/node.png'
 
@@ -18,15 +18,45 @@ export default class Projects extends Component {
                        <div className="row">
                             <div className="col-lg-4 col-md-4 col-md-6">
                                <div className="single-brand">
-                                   <img src="./"></img>
+                                   <img src={d} width="50%" height="50%" alt="html5-logo"></img>
+                               </div>
+                            </div>
+                            <div className="col-lg-4 col-md-4 col-md-6">
+                               <div className="single-brand">
+                                   <img src={css} width="50%" height="50%" alt="css-logo"></img>
                                </div>
                             </div> 
                             <div className="col-lg-4 col-md-4 col-md-6">
-                                
+                               <div className="single-brand">
+                                   <img src={js} width="50%" height="50%" alt="js-logo"></img>
+                               </div>
                             </div>  
                             <div className="col-lg-4 col-md-4 col-md-6">
-                                
-                            </div>    
+                               <div className="single-brand">
+                                   <img src={react} width="50%" height="50%" alt="react-logo"></img>
+                               </div>
+                            </div>  
+                            <div className="col-lg-4 col-md-4 col-md-6">
+                               <div className="single-brand">
+                                   <img src={reduxsaga} width="50%" height="50%" alt="reduxsaga-logo"></img>
+                               </div>
+                            </div>  
+                            <div className="col-lg-4 col-md-4 col-md-6">
+                               <div className="single-brand">
+                                   <img src={reactnative} width="50%" height="50%" alt="reactnative-logo"></img>
+                               </div>
+                            </div>  
+                            <div className="col-lg-4 col-md-4 col-md-6">
+                               <div className="single-brand">
+                                   <img src={angular} width="50%" height="50%" alt="angular-logo"></img>
+                               </div>
+                            </div>  
+                            <div className="col-lg-4 col-md-4 col-md-6">
+                               <div className="single-brand">
+                                   <img src={node} width="50%" height="50%" alt="node-logo"></img>
+                               </div>
+                            </div>  
+                               
                         </div> 
                     </div>
                     <div className="col-xl-6 col-lg-12 col-md-12">
