b8dacab42909a00fb2c22a494dd0ed681e15a4f1
abhaydee <abhaygirish1997@gmail.com>
2020-02-14 13:03:30 +0530

    added projects to work page

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import mm1 from '../img/macmm1.png'
 import classic1 from '../img/classic1.png'
+import Pizzeria from '../img/pizzeria.png'
+import myreads from '../img/myreads.png'
 import '../App.css'
 export default class Work extends Component {
     render() {
@@ -24,16 +26,17 @@ export default class Work extends Component {
                         </div>
                         <div className="col-md-6 col-sm-12 hover">
                             <a className="click" href="#">
-                                <img className="project-image" alt="memory-game" src={mm1} ></img>
-                                <h2 className="project-name">Memory-Game</h2>
+                                <img className="project-image" alt="Pizzeria" src={Pizzeria} ></img>
+                                <h2 className="project-name">Pizzeria</h2>
                             </a>
                         </div>
                         <div className="col-md-6 col-sm-12 hover">
                             <a className="click" href="#">
-                                <img className="project-image" alt="classic-arcade-game" src={classic1} ></img>
-                                <h2 className="project-name">Classic-Arcade-Game</h2>
+                                <img className="project-image" alt="React-Myreads" src={myreads} ></img>
+                                <h2 className="project-name">React-Myreads</h2>
                             </a>
                         </div>
+                        
                     </div>
                 </div>
             </div>
